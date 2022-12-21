93b21990f55597079a53e1338f71e105519db009
Elias Koromilas <elias.koromilas@gmail.com>
2022-12-21 12:09:24 +0200

    Update Intel HEXL dependency

--- cmake/ExternalIntelHEXL.cmake
+++ cmake/ExternalIntelHEXL.cmake
@@ -5,7 +5,7 @@ FetchContent_Declare(
     hexl
     PREFIX hexl
     GIT_REPOSITORY https://github.com/intel/hexl
-    GIT_TAG 0858760 # 1.2.3
+    GIT_TAG b4589b6 # 1.2.4
 )
 FetchContent_GetProperties(hexl)
 
